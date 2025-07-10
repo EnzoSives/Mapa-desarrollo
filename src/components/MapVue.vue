69c41458d528e6731cc0c80d40db322eededb817
Enzo Sives <enzosives7@gmail.com>
2025-07-09 23:47:44 -0300

    Zoom del mapa

--- src/components/MapVue.vue
+++ src/components/MapVue.vue
@@ -1293,7 +1293,7 @@ onMounted(() => {
     ],
     view: new View({
       center: fromLonLat([-57.1339, -37.0017]),
-      zoom: 17, // más cerca para ver numeración
+      zoom: 15, // más cerca para ver numeración
       minZoom: 14,
       maxZoom: 20,
       extent: extent,
